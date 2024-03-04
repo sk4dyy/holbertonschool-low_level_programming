2812f7c1ef956314bd74195c59435aa1af9ef676
sk4dyy <159240832+sk4dyy@users.noreply.github.com>
2024-03-04 10:58:56 +0000

    commit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,12 +11,14 @@ int sign = 1;
 unsigned int num = 0;
 do {
 if (*s == '-')
+
 sign *= -1;
 else if (*s >= '0' && *s <= '9')
 num = (num * 10) + (*s - '0');
 else if (num > 0)
 break;
-} while (*s++);
+}
+while (*s++);
 
 return (num *sign);
 
