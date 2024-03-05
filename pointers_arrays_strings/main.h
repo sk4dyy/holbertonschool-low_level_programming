0f5b642c8059dee91cec2ed3dd1746ceb114c785
sk4dyy <159240832+sk4dyy@users.noreply.github.com>
2024-03-05 13:12:33 +0000

    commit

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,6 +20,7 @@ char *cap_string(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 
 
 
