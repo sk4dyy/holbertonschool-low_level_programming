1a3ebea02b8a8bd015105dc6db680c1fb2532eae
sk4dyy <159240832+sk4dyy@users.noreply.github.com>
2024-03-05 14:48:50 +0000

    commit

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,12 +10,9 @@ void puts_half(char *str)
 	index = 0;
 	while (str[index] != '\0')
 		index++;
-	
 	half = index / 2;
-	
 	if (index % 2 == 1)
 		half++;
-	
 	while (half < index)
 	{
 		_putchar(str[half]);
