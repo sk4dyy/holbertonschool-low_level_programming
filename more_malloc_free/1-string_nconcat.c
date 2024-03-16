46d6010967f9888ead806da0fbd61e555207f105
sk4dyy <159240832+sk4dyy@users.noreply.github.com>
2024-03-16 21:29:36 +0000

    updates

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,34 +1,34 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * string_nconcat - concatenate 2 strings, only n bytes of s2
- * @s1: string 1
- * @s2: string 2
+ * @a: string 1
+ * @b: string 2
  * @n: bytes to include of s2
  * Return: NULL if fail, else pointer to malloc memory
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *a, char *b, unsigned int n)
 {
 char *p;
 unsigned int strlen1, i, c;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+if (a == NULL)
+a = "";
+if (b == NULL)
+b = "";
 
-strlen1 = _strlen(s1);
+strlen1 = _strlen(a);
 p = malloc((strlen1 + n + 1) * sizeof(char));
 if (p == NULL)
 return (NULL);
 for (i = 0, c = 0; i < (strlen1 + n); i++)
 {
 if (i < strlen1)
-p[i] = s1[i];
+p[i] = a[i];
 else
-p[i] = s2[c++];
+p[i] = b[c++];
 }
 p[i] = '\0';
 
