3e0fa150b69eb7ebc43169f4863f12680a4983db
sk4dyy <159240832+sk4dyy@users.noreply.github.com>
2024-03-16 21:35:35 +0000

    updates

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,4 +1,5 @@
-#include <stdlib.h>
-int _putchar(char c);
 void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *a, char *b, unsigned int n);
+int _strlen(char *s);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
