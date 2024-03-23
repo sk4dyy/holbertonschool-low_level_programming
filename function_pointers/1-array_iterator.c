9cba06d2850f5bc2a8b9bd1ca4d3babf3284c4d0
sk4dyy <159240832+sk4dyy@users.noreply.github.com>
2024-03-23 21:38:03 +0000

    1.2.3

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - execute a function 
+ * @array: array of integers
+ * @size: size of array
+ * @action: function pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
